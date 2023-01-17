f6b3b46abd81e8e3a99c25eb6a42bac062d92dfe
Pritam <pritam1813@gmail.com>
2023-01-17 17:39:54 +0530

    Added Sign Up and Sign In page and defined their routes

--- contollers/users_controller.js
+++ contollers/users_controller.js
@@ -4,3 +4,11 @@
 module.exports.profile = function(req,res){
     return res.render('profile', { title : 'Profile'});
 };
+
+module.exports.signup = function(req,res){
+    return res.render('SignUp', { title : 'Sign Up'});
+};
+
+module.exports.login = function(req,res){
+    return res.render('Login', { title : 'Sign In'});
+};
--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,11 @@ const router = express.Router();
 //Importing User Controller
 const userController = require('../contollers/users_controller');
 
-//Using GET Method to access Profile Route
+//Using GET Method to access Different User Routes
 router.get('/profile', userController.profile);
+router.get('/signup', userController.signup);
+router.get('/login', userController.login);
+
 
 //Exporting this router to make it accessible by main index Router
 module.exports = router;
