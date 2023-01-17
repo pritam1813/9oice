105207d744620574e114f393a21bef0366f587b0
Pritam <pritam1813@gmail.com>
2023-01-17 10:41:43 +0530

    Created a controller and defined root route

---
+++ contollers/home_conroller.js
@@ -0,0 +1,6 @@
+/* Responsible for different actions within a Route */
+
+
+module.exports.home = function(req,res){
+    return res.end("<h1>For Express App</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const express = require('express');
 //Including express.Router function for better routing
 const router = express.Router();
 
+//Importing home_controller from the controller folder
+const homeController = require('../contollers/home_conroller'); 
 
-
-
-
+//Defining GET actions for different Routes
+router.get('/', homeController.home);
 
 //Export router to use in main index.js
 module.exports = router;
