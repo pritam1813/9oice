69ed84e8ca1c1c1b24a9ed6f4ab2f6f0541855a1
Pritam <pritam1813@gmail.com>
2023-01-17 18:29:09 +0530

    Modfied Sign Up and Login pages added form action

--- contollers/users_controller.js
+++ contollers/users_controller.js
@@ -5,10 +5,22 @@ module.exports.profile = function(req,res){
     return res.render('profile', { title : 'Profile'});
 };
 
+//User Sign Up route for rendering Sign Up page
 module.exports.signup = function(req,res){
     return res.render('SignUp', { title : 'Sign Up'});
 };
 
+//User Sign In route for rendering Sign In page
 module.exports.login = function(req,res){
     return res.render('Login', { title : 'Sign In'});
+};
+
+//Action for Creating/Signing Up a user and storing the Data in the Database
+module.exports.create = function(req, res){
+    
+};
+
+//Action for Logging in a user and creating a Session
+module.exports.create_session = function(req, res){
+    
 };
