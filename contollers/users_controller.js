f8541ee5b4c8659bdb3fe4cb2cb5c4ab38d34c58
Pritam <pritam1813@gmail.com>
2023-01-17 16:17:17 +0530

    Added Layout for ejs and a profile page

--- contollers/users_controller.js
+++ contollers/users_controller.js
@@ -2,5 +2,5 @@
 
 //user profile route
 module.exports.profile = function(req,res){
-    return res.end("<h1>User Profile</h1>");
+    return res.render('profile', { title : 'Profile'});
 };
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Imports and const */
+
 //Require Express from Node Module 
 const express = require('express');
 
@@ -7,12 +9,21 @@ const port = 3000;
 //Creating Express app
 const app = express();
 
-//Importing router from routes folder index file and telling express app to use those routes
-app.use('/', require('./routes'));
+//Importing Express ejs Layout module
+const expressLayouts = require('express-ejs-layouts');
+
+
 
 //Setting View Engine
 app.set('view engine', 'ejs');
 app.set('views', './views'); //Setting views in the 'views' folder
+
+//Setting Layouts
+app.use(expressLayouts);
+
+//Importing router from routes folder index file and telling express app to use those routes
+app.use('/', require('./routes'));
+
 
 //Making the app to listen/run on default port
 app.listen(port, function(err) /*Callback Function*/{
