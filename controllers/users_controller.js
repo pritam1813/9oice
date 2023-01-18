9ddb3a66920a69908fb00b872bbbc73835f7166b
Pritam <pritam1813@gmail.com>
2023-01-18 14:27:43 +0530

    Added functions for Creating User

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,8 @@
 /* Responsible for different actions within user Route */
 
+//Importing Database Model 
+const User = require('../models/user');
+
 //user profile route
 module.exports.profile = function(req,res){
     return res.render('profile', { title : 'Profile'});
@@ -15,9 +18,31 @@ module.exports.login = function(req,res){
     return res.render('Login', { title : 'Sign In'});
 };
 
+
 //Action for Creating/Signing Up a user and storing the Data in the Database
 module.exports.create = function(req, res){
-    
+    //If the password and confirm password doesn't match then we will not create user
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    //Trying to find if the user with same email already exists in the database
+    User.findOne({ email: req.body.email}, function(err, user){
+        if(err){console.log('Error in Finding User while Sign Up'); return;} //Handling error for finding the user
+
+        //If User doesn't exist then creating the User
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in Creating User'); return;} //Handling error in creating user
+
+                return res.redirect('/user/login'); //If user is successfully created then redirecting user to Sign In page
+            });
+        } else {
+            return res.redirect('/user/login'); //If user already exists then redirecting to sign in page
+        }
+
+    });
+
 };
 
 //Action for Logging in a user and creating a Session
--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,9 @@ router.get('/profile', userController.profile);
 router.get('/signup', userController.signup);
 router.get('/login', userController.login);
 
+//Using POST Method to control different actions of sending the data to the server
+router.post('/create', userController.create);  // Creating a user/Sending registration data to the server
+
 
 //Exporting this router to make it accessible by main index Router
 module.exports = router;
