9f399bb68f283d1afc6b3dba22be101c93efccd3
Pritam <pritam1813@gmail.com>
2023-01-19 14:35:00 +0530

    Installed Passportjs

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get('/login', userController.login);
 
 //Using POST Method to control different actions of sending the data to the server
 router.post('/create', userController.create);  // Creating a user/Sending registration data to the server
-
+router.post('/create_session', userController.create_session);
 
 //Exporting this router to make it accessible by main index Router
 module.exports = router;
