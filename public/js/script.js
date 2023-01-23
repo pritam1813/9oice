e2064c6b0a62ec367b3ae9afbf68f65a03efe5da
Pritam <pritam1813@gmail.com>
2023-01-23 10:46:26 +0530

    Installed jquery for handling events

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+/* Nav Menu button events */
+$('#loginnav').click(function(){
+    location.href = '/user/login'
+});
+
+$('#signupnav').click(function(){
+    location.href = '/user/signup'
+});
+
+$('#logoutnav').click(function(){
+    location.href = '/user/logout'
+});
