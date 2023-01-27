81921d1c20ba196edc62b86d00a7b3e5e6d92f4f
Pritam <pritam1813@gmail.com>
2023-01-27 12:55:45 +0530

    Added checkAuthentication to restrict other users from creating a post

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const router = express.Router(); 
 const postsController = require('../controllers/posts_controller'); //Importing Post Controller
+const passport = require('passport');
 
-router.post('/create', postsController.create); //Creating Feed post for user
+router.post('/create', passport.checkAuthentication, postsController.create); //Creating Feed post for user
 
 module.exports = router;
