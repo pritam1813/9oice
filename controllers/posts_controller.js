15c8a0c670b34b8d98b0888d236f44bcdd2b1bbc
Pritam <pritam1813@gmail.com>
2023-01-28 17:25:54 +0530

    Added delete post function and button

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
-const Posts = require('../models/posts'); //Importing Post Schema
-
+const Posts = require('../models/posts');       //Importing Post Schema
+const Comment = require('../models/comment');   //Importing the Comment Schema
 
 //Action for handling user's Feed Post creation
 module.exports.create = function(req, res){
@@ -11,5 +11,29 @@ module.exports.create = function(req, res){
         if(err){console.log(`Error in Post Creation: ${err}`); return;}
 
         return res.redirect('back');
+    });
+};
+
+//Action for deleting a user post
+module.exports.destroy = function(req, res){
+
+    Posts.findById(req.params.id, function(err, post){  //Fetching post id from url param
+        if(err){console.log(err)};
+
+        //post.user is the user from posts schema which returns the user id.
+        /*Comparing if the currently authorized user id is same as the post.user 
+        So that, deleting permission for other users are restricted*/  
+        if(post.user == req.user.id){
+            post.remove();          //Deleting the post
+
+            //Deleting all/ the Comments of the post
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+
     });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const postsController = require('../controllers/posts_controller'); //Importing Post Controller
 const passport = require('passport');                               //Import Passport.js
 
-router.post('/create', passport.checkAuthentication, postsController.create); //Creating Feed post for user
+router.post('/create', passport.checkAuthentication, postsController.create);       //Creating Feed post for user
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);  //Deleting user post
 
 module.exports = router;
