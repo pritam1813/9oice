73d93b598714e1d186d1c489b2b399d274d95ac3
Pritam <pritam1813@gmail.com>
2023-02-17 15:27:56 +0530

    Upgraded aws-sdk v2 to v3

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,7 +4,7 @@
 const User = require('../models/user');                                 //Database Model user
 const ResetuserPassword = require('../models/reset_password_token');
 require('dotenv').config();                                             //env module for hiding sensitive info
-const AWS = require('aws-sdk');                                         //aws-sdk for s3 bucket storage access
+const {S3} = require("@aws-sdk/client-s3");                             //aws-sdk for s3 bucket storage access
 const { v4: uuidv4 } = require('uuid');                                 //To Generate RFC-compliant UUIDs for file names
 const AVATAR_BUCKET = '9oice';                                          //s3 bucket name
 const AVATAR_FOLDER = 'uploads/Avatars';                                //s3 folder path
@@ -16,9 +16,12 @@ const emailWorker = require('../workers/emails_worker');                //Worker
 const crypto = require('node:crypto');                                  //crypto module for generating random String
 
 // Configuring the AWS SDK
-const s3 = new AWS.S3({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+const s3 = new S3({
+    region: 'auto',
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
+    },
     endpoint: process.env.ENDPOINT,
     signatureVersion: 'v4'
 });
@@ -132,32 +135,29 @@ module.exports.update = async function(req, res){
                     };
 
                     //If user has already an avatar then deleting it
-                    if(user.avatar){
+                    if(user.rawAvatarURL){
                         //Function to delete s3 object
-                        s3.deleteObject({Bucket: AVATAR_BUCKET, Key: user.rawAvatar}, 
+                        s3.deleteObject({Bucket: AVATAR_BUCKET, Key: user.rawAvatarURL}, 
                             function(err ,data){
                             if (err) console.log(err, err.stack); // an error occurred
-                            console.log(data);                    // successful response
                         });
                     }
 
                     //Uploading the file
-                    s3.upload(params, function(err, data){
+                    s3.putObject(params, function(err){
                         if (err) {
                             console.log('Error uploading file: ', err);
                             req.flash('error', err);
                             return res.redirect('back');
                         }
-
-                        user.avatar = process.env.PUBURL + `/${data.key}`;       //Saving object public url in db
-                        user.rawAvatarURL = data.key;                              //Saving the key for deleting the object later
-                        user.save();
-                        return res.redirect('back');
+                            
                     });
-                } else {
-                    user.save();
-                    return res.redirect('back');
+                    user.avatar = process.env.PUBURL + `/${key}`;           //Saving object public url in db
+                    user.rawAvatarURL = `${key}`;                           //Saving the key for deleting the object later
                 }
+                await user.save();
+                
+                return res.redirect('back');
             });
 
         } catch(err) {
